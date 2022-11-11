5e1264b7edc30315a424920ca548abe2402ae0ff
McCain Carruthers <mbcarruthers@crimson.ua.edu>
2022-11-10 22:34:41 -0500

    boilerplate of a react18 app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+    return(
+        <div className="container text-center outline">
+            <h1>this is working</h1>
+        </div>
+    )
+}
+
+export default App;
