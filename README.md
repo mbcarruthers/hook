3f06018467bad3957718b763268fe1bf4f642c46
McCain Carruthers <mbcarruthers@crimson.ua.edu>
2022-11-20 13:10:51 -0500

    done

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hook 
+
+A blank application boilerplate with React18, Axios, Shipt/Osmosis,
+React Router DOM v6 , react-scripts, and bookstrap via CDN.
+
+A great boilerplate for any Web Development project centered around
+ReactJS.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const App = () => {
-    return(
-        <div className="container text-center outline">
-            <h1>this is working</h1>
-        </div>
-    )
+   return(
+       <div className="container-text-center outline m-2 p-1">
+           <h1>Blank Application</h1>
+       </div>
+   )
 }
 
 export default App;
